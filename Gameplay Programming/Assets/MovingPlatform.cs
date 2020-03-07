21055760d21048a4d7beaf2ccdfefa2e412dcc1d
Zoe <zoerowbotham1@gmail.com>
2020-03-07 16:33:26 +0000

    Platforms Moving Along Spline
    To Do:
    - Smooth "floating" movement
    - Stepped "mechanical" movement
    - Bobbing with Player
    - Parent Player Option

---
+++ Gameplay Programming/Assets/MovingPlatform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    MovingPlatformsController controller;
+    int current_index = 0;
+
+    bool move = false;
+
+    private void Update()
+    {
+        if (move)
+        {
+            Vector3 new_pos = Vector3.Lerp(transform.position, controller.spline_positions[current_index + 1], controller.platform_move_speed * Time.deltaTime);
+            transform.position = new Vector3(new_pos.x, transform.position.y, new_pos.z);
+
+            if (Vector3.Distance(transform.position, controller.spline_positions[current_index + 1]) < 0.2f)
+            {
+                current_index += 1;
+
+                if (current_index == controller.spline_positions.Count - 1)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+
+    public void StartPlatform(MovingPlatformsController platform_controller)
+    {
+        controller = platform_controller;
+        move = true;
+    }
+}
---
+++ Gameplay Programming/Assets/MovingPlatformsController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MovingPlatformsController : MonoBehaviour
+{
+    public PathCreator platform_spline;
+
+    public Transform platforms_parent;
+    public GameObject platform_prefab;
+    public float distance_between_platforms;
+    public float platform_move_speed;
+    public int bezier_position_divisions = 10;
+
+    [HideInInspector]
+    public List<Vector3> spline_positions = new List<Vector3> { };
+    List<MovingPlatform> platforms = new List<MovingPlatform> { };
+
+    float timer = 0;
+    float time_between_spawns = 10;
+    bool triggered = true;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        time_between_spawns = distance_between_platforms / platform_move_speed;
+
+        // Get Spline Points
+        for (int i = 0; i < platform_spline.path.NumSegments; i++)
+        {
+            Vector3[] points = platform_spline.path.GetPointsInSegment(i);
+            Vector3[] new_positions = Handles.MakeBezierPoints(points[0], points[3], points[1], points[2], bezier_position_divisions);
+            spline_positions.AddRange(new_positions);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggered)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > time_between_spawns)
+            {
+                timer = 0;
+                MovingPlatform new_platform = Instantiate(platform_prefab, spline_positions[0], Quaternion.identity, platforms_parent).GetComponent<MovingPlatform>();
+                new_platform.StartPlatform(this);
+                platforms.Add(new_platform);
+            }
+        }
+    }
+
+    public void StartPlatforms()
+    {
+        triggered = true;
+    }
+
+    public void StopPlatforms()
+    {
+        triggered = false;
+    }
+}
