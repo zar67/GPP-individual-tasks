13de0777761e4ff92ccd4007a1f50af6c8362a36
Zoe <zoerowbotham1@gmail.co.uk>
2020-03-12 12:40:00 +0000

    Player Can Be Damaged

---
+++ Gameplay Programming/Assets/Scripts/Avatar/AttackManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackManager : MonoBehaviour
+{
+    [HideInInspector]
+    public GameObject collided = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collided = other.gameObject;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        collided = null;
+    }
+}
